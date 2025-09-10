1dfef910fe0fae3360c08716d31c756a04564b78
Kiloni Quiles <kiloni.quiles@rutgers.edu>
2025-09-10 12:54:26 -0400

    Updating signature script

--- Signatures/BugSigDB_Microbial_Signatures.R
+++ Signatures/BugSigDB_Microbial_Signatures.R
@@ -5,6 +5,9 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) {
 BiocManager::install("bugsigdbr")
 
 library(bugsigdbr)
+library(tidyverse)
+library(reshape2)
+library(pheatmap)
 
 #Import the BugSigDB Dataset
 bsdb <- importBugSigDB()
@@ -21,6 +24,21 @@ nutrition_keywords <- c("diet", "nutrition", "fiber", "fat", "carbohydrate", "pr
 nutrition_conditions <- all_conditions[
   grepl(paste(nutrition_keywords, collapse = "|"), all_conditions, ignore.case = TRUE)
 ]
-print(nutrition_conditions)
 
+# Print and save a CSV of the nutrition-related conditions                                 
+print(nutrition_conditions)
 write.csv(data.frame(Nutrition_Conditions = nutrition_conditions), "Nutrition_Conditions_BugSigDB_Conditions.csv", row.names = FALSE)
+
+# Filter studies with nutrition-related conditions
+nutrition_studies <- bsdb[grepl(paste(nutrition_keywords, collapse = "|"), bsdb$Condition, ignore.case = TRUE),
+]
+
+# Extract microbial signatures (species-level)
+nutrition_signatures <- getSignatures(nutrition_studies, tax.id.type = "taxname", tax.level = "species")
+
+# View the signature results
+str(nutrition_signatures)
+
+# Save to a GMT file for enrichment analysis
+writeGMT(nutrition_signatures, "nutrition_signatures_bugsigdb.gmt")
+
