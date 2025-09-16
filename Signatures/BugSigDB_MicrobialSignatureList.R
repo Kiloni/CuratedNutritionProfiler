fd301de5ee6f87e5e84afd0379a175bdcde19317
Kiloni Quiles <kiloni.quiles@rutgers.edu>
2025-09-16 09:41:24 -0400

    Updating microbial signatures

--- Signatures/BugSigDB_MicrobialSignatureList.R
+++ Signatures/BugSigDB_MicrobialSignatureList.R
@@ -4,5 +4,12 @@ MicrobeSignature = list(
   "Bacteroides_fragilis" = c("bfe1", "ermF", "tetQ", "recA", "rpoB", "ompA", 
                              "btuB", "gdhA", "fabl", "bfr"),
   "Lactobacillus_plantarum" = c("bsh", "plnEF", "groEL", "clpP", "rex", "ldh", 
-                                "epsA-E", "glnA", "xylA", "hsp1")
+                                "epsA-E", "glnA", "xylA", "hsp1"),
+  "Alistipes_putredinis" = c("fabZ", "pyrG", "kbl", "rlmN", "coaBC", "asd"),
+  "Roseburia_intestinalis" = c("rpoB"),
+  "Bifidobacterium_adolescentis" = c("bgaB", "BAD_1030", "sucP", "carB", "glmU"),
+  "Akkermansia_muciniphila" = c("Amuc_2018", "Amuc_0868", "carA", "ftsH", "rlmN", 
+                                "ilvC"),
+  "Eubacterium_rectale" = c("but", "rpoB"),
+  "Bilophila_wadsworthia" = c("tpa")
 )
